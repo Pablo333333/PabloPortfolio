a01ee2c02022b0ccd3cffc6fafd693c330e9dc85
Pablo333333 <pj669349@gmail.com>
2023-02-12 12:32:39 -0300

    hangman

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import videogames from "../assets/portfolio/videogames.png";
+import hangman from "../assets/portfolio/hangman.png"
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
       src: videogames,
+      link: "https://pi-videogames-pablo.vercel.app/",
+      code: "https://github.com/Pablo333333/PI-Videogames-main",
+    },
+    {
+      id: 2,
+      src: hangman,
+      link: "https://hangman-pablo.vercel.app/",
+      code: "https://github.com/Pablo333333/Hangman",
     },
   ];
 
@@ -23,7 +32,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, link, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -32,10 +41,10 @@ const Portfolio = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  <a href= "https://pi-videogames-pablo.vercel.app/">Demo </a>
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  <a href= "https://github.com/Pablo333333/PI-Videogames-main">Code </a>
+                  <a href={link}>Demo </a>
+               </button>
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href= {code}>Code </a>
                 </button>
               </div>
             </div>
