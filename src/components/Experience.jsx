a0cdbedda53ad3f6d778b7419bb894f6b68d0142
Pablo333333 <pj669349@gmail.com>
2023-03-22 16:51:31 -0300

    upload portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import javascript from "../assets/javascript.png";
+import nodeJs from "../assets/nodejs.png";
+import typescript from "../assets/typescript.png";
+import postgreSQL from "../assets/postgreSQL.png";
 import reactImage from "../assets/react.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
@@ -11,21 +11,21 @@ const Experience = () => {
   const techs = [
     {
       id: 1,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: nodeJs,
+      title: "NodeJs",
+      style: "shadow-green-500",
     },
     {
       id: 2,
-      src: css,
-      title: "CSS",
+      src: typescript,
+      title: "Typescript",
       style: "shadow-blue-500",
     },
     {
       id: 3,
-      src: javascript,
-      title: "JavaScript",
-      style: "shadow-yellow-500",
+      src: postgreSQL,
+      title: "PostgreSQL",
+      style: "shadow-blue-500",
     },
     {
       id: 4,
